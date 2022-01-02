fa95ec5e8b26653845041c77b6f95becb1eb1e62
nhatanh1112 <nguyenvonhatanh012345@gmail.com>
2022-01-03 00:22:46 +0700

    simple obj

--- ObjectGroup.java
+++ ObjectGroup.java
@@ -6,22 +6,6 @@ import java.util.ArrayList;
 public class ObjectGroup extends SimpleObject {
     protected final ArrayList<SimpleObject> objects = new ArrayList<>();
 
-    public void addObject(SimpleObject object) {
-        objects.add(object);
-    }
-
-    public void removeObject(SimpleObject object) {
-        objects.remove(object);
-    }
-
-    public void clear() {
-        objects.clear();
-    }
-
-    public ArrayList<SimpleObject> getObjects() {
-        return objects;
-    }
-
     @Override
     public void tick() {
         for (SimpleObject object : objects) {
