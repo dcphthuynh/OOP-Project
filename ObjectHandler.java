5e3ba1c8842c966695f113967794a263ba19e27e
nhatanh1112 <nguyenvonhatanh012345@gmail.com>
2022-01-05 23:55:09 +0700

    Object Handler

--- ObjectHandler.java
+++ ObjectHandler.java
@@ -4,6 +4,12 @@ import java.awt.*;
 import java.util.ArrayList;
 
 public class ObjectHandler {
+    /**
+	 * 
+	 */
+	/**
+	 * 
+	 */
     private static ObjectHandler objectHandler = null;
     private final ArrayList<SimpleObject> objects = new ArrayList<>();
     private final ArrayList<SimpleObject> objectsToRemove = new ArrayList<>();
@@ -14,5 +20,33 @@ public class ObjectHandler {
             objectHandler = new ObjectHandler();
         }
         return objectHandler;
+    }
+
+    // Tick method 
+    public void tick() {
+        for (SimpleObject object : objects) {
+            object.tick();
+        }
+
+        objects.removeAll(objectsToRemove);
+        objects.addAll(objectsToAdd);
+
+        objectsToAdd.clear();
+        objectsToRemove.clear();
+    }
+
+    // Render the image
+    public void render(Graphics2D graphic) {
+        for (SimpleObject object : objects) {
+            object.render(graphic);
+        }
+    }
+
+    public void addObject(SimpleObject object) {
+        objectsToAdd.add(object);
+    }
+
+    public void removeObject(SimpleObject object) {
+        objectsToRemove.add(object);
     }
 }
